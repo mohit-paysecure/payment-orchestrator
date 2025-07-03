552727c1dcf94f7fe8f0bdabff115adc76d02ca1
Mohit Bhagwani <mohitbhagwani@netgraph.business>
2025-07-03 15:48:16 +0530

    project setup done

--- src/main/java/com/paysecure/payment_orchestrator/repo/ProviderConfigRepository.java
+++ src/main/java/com/paysecure/payment_orchestrator/repo/ProviderConfigRepository.java
@@ -2,9 +2,11 @@ package com.paysecure.payment_orchestrator.repo;
 
 import com.paysecure.payment_orchestrator.entity.ProviderConfigEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.UUID;
 
+@Repository
 public interface ProviderConfigRepository extends JpaRepository<ProviderConfigEntity, UUID> {
     /**
      * Finds a ProviderConfigEntity by its provider name.
